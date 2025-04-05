908d8aee840bcd45d0758ef5e0b27df61b4b3e4e
Megh Bari <meghbari01@gmail.com>
2025-04-05 15:04:04 +0530

    functions

---
+++ 01 - Intro/functions.js
@@ -0,0 +1,12 @@
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(5);
+function getUpper(str) {
+    return str.toUpperCase();
+}
+getUpper("megh");
+function signUpUser(name, email, password) { }
+signUpUser("megh", "nothing@nothing.nothing", 1234);
+var loginUser = function (name, isPaid) { };
+loginUser("megh", false);
---
+++ 01 - Intro/functions.ts
@@ -0,0 +1,19 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+
+let myVal = addTwo(5);
+
+function getUpper(str: string) {
+  return str.toUpperCase();
+}
+
+getUpper("megh");
+
+function signUpUser(name: string, email: string, password: number) {}
+
+signUpUser("megh", "nothing@nothing.nothing", 1234);
+
+let loginUser = (name: string, isPaid: boolean) => {};
+
+loginUser("megh", false);
