f4bcc47c47a659dec75040365edf0af686ff4ec0
Megh Bari <meghbari01@gmail.com>
2025-04-05 14:44:48 +0530

    any keyword-don't use any

--- 01 - Intro/variables.ts
+++ 01 - Intro/variables.ts
@@ -18,4 +18,18 @@ let isLoggedIn: boolean = false
 
 isLoggedIn.valueOf() // true or false
 
+
+// any 
+// Idk what type this is
+
+let hero : any;
+
+function getHeros(){
+    return "Spiderman"
+}
+
+hero = getHeros()
+
+// But be careful — using any removes all the safety TypeScript gives you. It’s like turning off TypeScript for that variable
+//  but don't use any in any of your code
 export {}
