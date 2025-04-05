05d81c466a09fb83a9aaf39d89353069b6b25586
Megh Bari <meghbari01@gmail.com>
2025-04-05 15:31:54 +0530

    more better way to write functions

--- 01 - Intro/functions.ts
+++ 01 - Intro/functions.ts
@@ -1,5 +1,8 @@
-function addTwo(num: number) {
+// better way to write function
+
+function addTwo(num: number): number {
   return num + 2;
+  // return "hello"
 }
 
 let myVal = addTwo(5);
@@ -17,3 +20,32 @@ signUpUser("megh", "nothing@nothing.nothing", 1234);
 let loginUser = (name: string, isPaid: boolean) => {};
 
 loginUser("megh", false);
+
+// more than one type in function
+
+// function getVal(myVal: number):string{
+//   if (myVal > 5) return true;
+//   return "200 OK";
+// }
+
+const getHello = (s: string): string => {
+  return "hello";
+};
+
+const heros = ["spiderman", "thor", "ironman"];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+// void coz there is no return type
+
+function consoleError(errorMsg: string): void {
+  console.log(errorMsg);
+}
+
+// handling error use never
+
+function handleError(errorMsg: string): never {
+  throw new Error(errorMsg);
+}
