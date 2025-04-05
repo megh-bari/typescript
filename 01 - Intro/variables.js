10338b753212b27532c13de8c7a77aff0f77f97e
Megh Bari <meghbari01@gmail.com>
2025-04-05 14:37:30 +0530

    varibales with type

---
+++ 01 - Intro/variables.js
@@ -0,0 +1,11 @@
+// string
+var greetings = "Hello";
+greetings.toLocaleLowerCase();
+console.log(greetings);
+// number
+var userId = 1823;
+userId.toFixed();
+console.log(userId);
+// boolean
+var isLoggedIn = false;
+isLoggedIn.valueOf(); // true or false
---
+++ 01 - Intro/variables.ts
@@ -0,0 +1,21 @@
+// string
+
+let greetings: string = "Hello";
+greetings.toLocaleLowerCase();
+
+console.log(greetings);
+
+// number
+
+let userId: number = 1823;
+
+userId.toFixed();
+console.log(userId);
+
+// boolean
+
+let isLoggedIn: boolean = false
+
+isLoggedIn.valueOf() // true or false
+
+export {}
