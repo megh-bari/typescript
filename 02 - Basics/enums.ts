166dbad990155045393de08bd7d821b5466eeb93
Megh Bari <meghbari01@gmail.com>
2025-04-06 14:29:42 +0530

    enums

---
+++ 02 - Basics/enums.ts
@@ -0,0 +1,10 @@
+// restrict the users choice
+
+enum seatChoice {
+  AISLE = 10, // default start with zero or assigned the value
+  MIDDLE = 22,
+  WINDOW = 33,
+  FOURTH = 44,
+}
+
+const mySeat = seatChoice.WINDOW;
