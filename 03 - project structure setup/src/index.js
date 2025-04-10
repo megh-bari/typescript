b7944e783cfff949d2f5fd9c1b7859b07a9d022a
Megh Bari <meghbari01@gmail.com>
2025-04-10 10:58:12 +0530

    access modfires

--- 03 - project structure setup/src/index.js
+++ 03 - project structure setup/src/index.js
@@ -1,11 +1,48 @@
 console.log("typescript is here still");
 console.log("Hey");
 // classes
+// class User {
+//   public email: string;
+//   name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 var User = /** @class */ (function () {
     function User(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1; // cannot access outside the class
+        this.city = "";
     }
+    Object.defineProperty(User.prototype, "getRandomEmail", {
+        //  getter and setter
+        get: function () {
+            return "random".concat(this.email);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(User.prototype, "courseCount", {
+        get: function () {
+            return this._courseCount;
+        },
+        set: function (courseNum) {
+            if (courseNum <= 1) {
+                throw new Error("Course count should be more than 1");
+            }
+            this._courseCount = courseNum;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return User;
 }());
 var megh = new User("megh@megh.in", "megh");
+console.log(megh);
+// megh.city = "mumbai";
+// megh.city; // i can access but can't modify
+console.log(megh.city);
+console.log(megh.getRandomEmail);
--- 03 - project structure setup/src/index.ts
+++ 03 - project structure setup/src/index.ts
@@ -14,13 +14,35 @@ console.log("Hey");
 // }
 
 class User {
+  private _courseCount = 1; // cannot access outside the class
+
   readonly city: string = "";
-  constructor(
-    public email: string, public name: string
-  )
-     {}
+  constructor(public email: string, public name: string) {}
+  //  getter and setter
+  get getRandomEmail(): string {
+    return `random${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+
+    this._courseCount = courseNum;
+  }
 }
 
 const megh = new User("megh@megh.in", "megh");
+
+
+console.log(megh);
 // megh.city = "mumbai";
-megh.city; // i can access but can't modify
+// megh.city; // i can access but can't modify
+
+console.log(megh.city); 
+
+console.log(megh.getRandomEmail);
