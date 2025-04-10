00e82e3206f6bdb47dba5dabc3def3eea17cd204
Megh Bari <meghbari01@gmail.com>
2025-04-10 10:37:45 +0530

    public and private

--- 03 - project structure setup/src/index.ts
+++ 03 - project structure setup/src/index.ts
@@ -3,15 +3,24 @@ console.log("Hey");
 
 // classes
 
+// class User {
+//   public email: string;
+//   name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
-  city: string = "";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  readonly city: string = "";
+  constructor(
+    public email: string, public name: string
+  )
+     {}
 }
 
 const megh = new User("megh@megh.in", "megh");
-megh.city = "mumbai";
+// megh.city = "mumbai";
+megh.city; // i can access but can't modify
