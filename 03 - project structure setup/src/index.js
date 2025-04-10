c9b6b24d5afdd90c37e65b7996c1d7da4dbd54c5
Megh Bari <meghbari01@gmail.com>
2025-04-10 10:29:55 +0530

    Classes

---
+++ 03 - project structure setup/src/index.js
@@ -0,0 +1,11 @@
+console.log("typescript is here still");
+console.log("Hey");
+// classes
+var User = /** @class */ (function () {
+    function User(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+    return User;
+}());
+var megh = new User("megh@megh.in", "megh");
--- 03 - project structure setup/src/index.ts
+++ 03 - project structure setup/src/index.ts
@@ -1,2 +1,17 @@
 console.log("typescript is here still");
 console.log("Hey");
+
+// classes
+
+class User {
+  email: string;
+  name: string;
+  city: string = "";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const megh = new User("megh@megh.in", "megh");
+megh.city = "mumbai";
