710b0f8c6699bd52407cde8f1395d80392431a05
Megh Bari <meghbari01@gmail.com>
2025-04-18 15:16:46 +0530

    2 types of narrowing

--- 03 - project structure setup/src/detection.ts
+++ 03 - project structure setup/src/detection.ts
@@ -50,3 +50,79 @@ function isAdminAccount(account: User | Admin) {
   }
   return false;
 }
+
+// instance-of
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+// Using type predicates
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
+
+// Discriminated unions
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  } else if (shape.kind === "rectangle") {
+    return shape.length * shape.width;
+  }
+  return shape.side * shape.side;
+}
+
+// The never type
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+
+    case "square":
+      return shape.side * shape.side;
+
+    case "rectangle":
+      return shape.length * shape.width;
+
+    default:
+      const _defaultForShape: never = shape;
+      return _defaultForShape;
+  }
+}
